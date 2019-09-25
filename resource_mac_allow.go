93a68bfd24868bb03b80976afc530691d80702b0
Claude Dioudonnat <claude.dioudonnat1@uca.fr>
2019-09-25 19:09:56 +0200

    Add and remove mac in allowed list

--- resource_mac_allow.go
+++ resource_mac_allow.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"log"
 	"net"
+	"strings"
 
 	"github.com/hashicorp/terraform/helper/schema"
 )
@@ -24,32 +26,47 @@ func resourceMacAllow() *schema.Resource {
 				Optional: true,
 				Default:  "",
 			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"mac_windows": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
 func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
+
+	c := m.(*Communicator)
+	c.Connect()
+
 	var mac net.HardwareAddr
 
 	if d.Get("mac") == "" {
 		log.Printf("[DEBUG] generate a mac address")
-		mac = GenerateMac()
+		mac = GenerateMac(c.GetAllAllowedMacAddress())
 	}
 
 	var err error
 
-	if d.Get("mac") != nil {
+	if d.Get("mac") != "" {
 		log.Printf("[DEBUG] validate a mac address")
 		mac, err = net.ParseMAC(d.Get("mac").(string))
 		if err != nil {
 			log.Printf("[DEBUG] invalid mac address")
+			return errors.New("Invalid mac Address")
 		}
-
 	}
 
-	c := m.(*Communicator)
-	c.Connect()
-	c.Run()
+	d.Set("mac_windows", NormalizeMacWindows(mac.String()))
+
+	error := c.AddFilterAllowAddress(d.Get("mac_windows").(string), d.Get("description").(string))
+	if error != nil {
+		return error
+	}
 	d.SetId(mac.String())
 	return nil
 }
@@ -63,7 +80,9 @@ func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
+	c := m.(*Communicator)
+	c.Connect()
+	return c.RemoveFilterAllowAddress(d.Get("mac_windows").(string))
 }
 
 func randomHex(n int) (string, error) {
@@ -74,9 +93,21 @@ func randomHex(n int) (string, error) {
 	return hex.EncodeToString(bytes), nil
 }
 
-func GenerateMac() net.HardwareAddr {
+func GenerateMac(except []string) net.HardwareAddr {
 	mac, _ := net.ParseMAC(subGenerate("00", 5))
+	if containsMac(NormalizeMacWindows(mac.String()), except) {
+		return GenerateMac(except)
+	}
 	return mac
+}
+
+func containsMac(mac string, macs []string) bool {
+	for _, n := range macs {
+		if mac == n {
+			return true
+		}
+	}
+	return false
 }
 
 func subGenerate(before string, count int) string {
@@ -86,4 +117,8 @@ func subGenerate(before string, count int) string {
 	sub, _ := randomHex(1)
 	new := before + ":" + sub
 	return subGenerate(new, count-1)
+}
+
+func NormalizeMacWindows(mac string) string {
+	return strings.ToUpper(strings.Replace(mac, ":", "-", -1))
 }
--- winrm.go
+++ winrm.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/masterzen/winrm"
 )
+
+type WinrmError struct {
+	codeReturn int
+	message    string
+	stderr     string
+}
+
+func (e *WinrmError) Error() string {
+	return fmt.Sprintf("Winrm (%d) - %s", e.codeReturn, e.message)
+}
 
 type Communicator struct {
 	username string
@@ -29,9 +42,53 @@ func (c *Communicator) Connect() error {
 	return nil
 }
 
-func (c *Communicator) Run() error {
-	stdout, stderr, _, _ := c.client.RunWithString("ipconfig /all", "")
+func (c *Communicator) AddFilterAllowAddress(mac string, description string) error {
+	command := fmt.Sprintf(
+		"Add-DhcpServerv4Filter -List Allow -macAddress \"%s\" -Description \"%s\"",
+		mac,
+		description,
+	)
+
+	_, stderr, returnCode := c.Execute(command)
+
+	if returnCode != 0 {
+		return &WinrmError{returnCode, "Cannot allow mac address in dhcp, maybe already allowed.", stderr}
+	}
+
+	return nil
+}
+
+func (c *Communicator) RemoveFilterAllowAddress(mac string) error {
+	command := fmt.Sprintf(
+		"Remove-DhcpServerv4Filter \"%s\"", mac,
+	)
+
+	c.Execute(command)
+
+	return nil
+}
+
+func (c *Communicator) GetAllAllowedMacAddress() []string {
+	stdout, _, _ := c.Execute("Get-DhcpServerv4Filter -List Allow")
+	lines := strings.Split(stdout, "\n")
+
+	var macs []string
+
+	re := regexp.MustCompile(`(([0-9ABCDEF]{2})-?){6,8}`)
+
+	for _, element := range lines {
+		matched, _ := regexp.MatchString(`^(([0-9ABCDEF]{2})-?){6,8}`, element)
+		if matched {
+			mac := string(re.Find([]byte(element)))
+			macs = append(macs, mac)
+		}
+	}
+	return macs
+}
+
+func (c *Communicator) Execute(command string) (string, string, int) {
+	stdout, stderr, returnCode, _ := c.client.RunWithString(winrm.Powershell(command), "")
 	log.Printf(stdout)
 	log.Printf(stderr)
-	return nil
+	return stdout, stderr, returnCode
 }
