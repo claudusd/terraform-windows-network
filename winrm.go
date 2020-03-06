705dced1ac90df3fe1b17611c9d8900b0c73521e
Claude Dioudonnat <claude.dioudonnat1@uca.fr>
2020-03-06 13:42:20 +0100

    Fix build error

--- winrm.go
+++ winrm.go
@@ -118,13 +118,15 @@ func (c *Communicator) RemoveDHCPReservation(mac string, scopeId string) error {
 	return nil
 }
 
-func (c *Communicator) RemoveDHCPLease(scopeId string, mac string, ip string)  {
-	command != fmt.Sprintf(
+func (c *Communicator) RemoveDHCPLease(scopeId string, mac string, ip string) error {
+	command := fmt.Sprintf(
 		"Remove-DhcpServerv4Lease -ScopeId %s -ClientId \"%s\" -IPAddress %s",
-		scopeId, mac, ip
+		scopeId, mac, ip,
 	)
 
 	c.Execute(command)
+
+	return nil
 }
 
 func (c *Communicator) GetFreeIp(scopeId string) (net.IP, error) {
